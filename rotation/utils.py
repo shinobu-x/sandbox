4bc1348fd4b341f33c557fa6f75cccc9852a31fd
shinobu.kinjo <shinobu.kinjo@GM10342.local>
2022-01-05 09:28:25 +0900

    modified:   rotation/utils.py 	modified:   rotation/visualize_rotation.py

--- rotation/utils.py
+++ rotation/utils.py
@@ -5,6 +5,24 @@ from mpl_toolkits.mplot3d import Axes3D
 from sympy import Symbol, var, init_printing
 from sympy.matrices import Matrix
 
+d0 =  [199,  21, 133] # MediumVioletRed
+d1 =  [220,  20,  60] # Crimson
+d2 =  [255,  99,  71] # Tomato
+d3 =  [255, 218, 185] # PeachPuff
+d4 =  [255, 228, 181] # Moccasin
+d5 =  [ 34, 139,  34] # ForestGreen
+d6 =  [  0, 255, 255] # Aqua
+d7 =  [ 30, 144, 255] # DodgerBlue
+d8 =  [135, 206, 235] # SkyBlue
+d9 =  [ 47,  79,  79] # DarkSlateGray
+d10 = [245, 255, 250] # MintCream
+d11 = [253, 245, 230] # OldLace
+d12 = [255, 228, 225] # MistyRose
+d13 = [218, 112, 214] # Orchid
+d14 = [138,  43, 226] # VlueViolet
+d15 = [ 75,   0, 130] # Indigo                                                                                                      
+colors_list = [[d0],[d1],[d2],[d3],[d4],[d5],[d6],[d7],[d8],[d9],[d10],[d11],[d12],[d13],[d14],[d15]]
+
 def coordination_2d(axes, range_x, range_y, grid = True,
                xyline = True, xlabel = "x", ylabel = "y"):
     axes.set_xlabel(xlabel, fontsize = 16)
@@ -14,8 +32,8 @@ def coordination_2d(axes, range_x, range_y, grid = True,
     if grid == True:
         axes.grid()
     if xyline == True:
-        axes.axhline(0, color = "gray")
-        axes.axvline(0, color = "gray")
+        axes.axhline(0.5, color = "black")
+        axes.axvline(0.5, color = "black")
 
 def coordination_3d(axes, range_x, range_y, range_z, grid = True):
     axes.set_xlabel("x", fontsize = 16)
--- rotation/visualize_rotation.py
+++ rotation/visualize_rotation.py
@@ -21,22 +21,27 @@ def pixel_checker(image):
     cv2.setMouseCallback('pixel check',on_click,image)                         
     cv2.waitKey(0)
 
-d0 =  [199,  21, 133] # MediumVioletRed
-d1 =  [220,  20,  60] # Crimson
-d2 =  [255,  99,  71] # Tomato
-d3 =  [255, 218, 185] # PeachPuff
-d4 =  [255, 228, 181] # Moccasin
-d5 =  [ 34, 139,  34] # ForestGreen
-d6 =  [  0, 255, 255] # Aqua
-d7 =  [ 30, 144, 255] # DodgerBlue
-d8 =  [135, 206, 235] # SkyBlue
-d9 =  [ 47,  79,  79] # DarkSlateGray
-d10 = [245, 255, 250] # MintCream
-d11 = [253, 245, 230] # OldLace
-d12 = [255, 228, 225] # MistyRose
-d13 = [218, 112, 214] # Orchid
-d14 = [138,  43, 226] # VlueViolet
+d0 =  [133,  21, 199] # MediumVioletRed
+d1 =  [60,  20,  220] # Crimson
+d2 =  [71,  99,  255] # Tomato
+d3 =  [185, 218, 255] # PeachPuff
+d4 =  [181, 228, 255] # Moccasin
+d5 =  [34,  139,  34] # ForestGreen
+d6 =  [255, 255,   0] # Aqua
+d7 =  [155, 144,  30] # DodgerBlue
+d8 =  [235, 206, 135] # SkyBlue
+d9 =  [ 79,  79,  47] # DarkSlateGray
+d10 = [250, 255, 245] # MintCream
+d11 = [230, 245, 253] # OldLace
+d12 = [225, 228, 255] # MistyRose
+d13 = [214, 112, 218] # Orchid
+d14 = [226,  43, 138] # VlueViolet
+d15 = [ 130,  0,  75] # Indigo
+
+'''
+d0 =  [199,  21, 133] # MediumVioletRed                                                                                             d1 =  [220,  20,  60] # Crimson                                                                                                     d2 =  [255,  99,  71] # Tomato                                                                                                      d3 =  [255, 218, 185] # PeachPuff                                                                                                   d4 =  [255, 228, 181] # Moccasin                                                                                                    d5 =  [ 34, 139,  34] # ForestGreen                                                                                                 d6 =  [  0, 255, 255] # Aqua                                                                                                        d7 =  [ 30, 144, 255] # DodgerBlue                                                                                                  d8 =  [135, 206, 235] # SkyBlue                                                                                                     d9 =  [ 47,  79,  79] # DarkSlateGray                                                                                               d10 = [245, 255, 250] # MintCream                                                                                                   d11 = [253, 245, 230] # OldLace                                                                                                     d12 = [255, 228, 225] # MistyRose                                                                                                   d13 = [218, 112, 214] # Orchid                                                                                                      d14 = [138,  43, 226] # VlueViolet                                                                                                  
 d15 = [ 75,   0, 130] # Indigo
+'''
 colors_list = [[d0],[d1],[d2],[d3],[d4],[d5],[d6],[d7],[d8],[d9],[d10],[d11],[d12],[d13],[d14],[d15]]
 factor = 8
 unit_vector = (1, 0)
@@ -44,7 +49,8 @@ fig, ax = plt.subplots(1, 1, figsize=(4,4))
 fig.patch.set_alpha(0)
 ax.axes.xaxis.set_ticks([])
 ax.axes.yaxis.set_ticks([])
-coordination_2d(ax, [-1.5, 1.5], [-1.5, 1.5], grid=False, xyline=False)
+#coordination_2d(ax, [-1.5, 1.5], [-1.5, 1.5], grid=False, xyline=False)
+coordination_2d(ax, [-1.5,1.5],[-1.5,1.5],grid=True,xyline=True)
 rotated_vector = rotation(unit_vector, np.pi/factor)
 for i in range(360//(180//factor)):
     rotated_vector = rotation(rotated_vector, np.pi/factor)
